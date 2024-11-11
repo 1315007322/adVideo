f11a3827420df250eb3f5c8e35506f67bd680049
yanhengfu <1315007322@qq.com>
2024-11-11 16:53:31 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,87 @@
 <template>
   <div class="ads">
-    <van-submit-bar
-        label="我的钱包"
-        :price="Number(moneny) * 100"
-        :decimal-length="2"
-        button-text="提现"
-        @submit="onSubmit"
-    />
-    <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
-      <van-list
-          v-model:loading="loading"
-          :finished="finished"
-          finished-text="没有更多了"
-          @load="onLoad"
-      >
-        <van-cell-group inset center>
-          <van-cell
-              v-for="(item, index) in list"
-              :label="item.des"
-          >
-            <template #title>
-              <p>{{ item.title }} <span style="padding: 10px"> + {{ item.amount }}</span></p>
-            </template>
-            <!-- 使用 right-icon 插槽来自定义右侧图标 -->
-            <template #icon>
-              <van-image
-                  style="margin-right: 10px"
-                  width="40"
-                  height="40"
-                  src="https://file-1330837989.cos.ap-guangzhou.myqcloud.com//dy.png"
-              />
-            </template>
-            <!-- 使用 right-icon 插槽来自定义右侧图标 -->
-            <template #value>
+    <div v-if="!isLogin" class="login">
 
-              <van-button
-                  size="small"
-                  @click="() => openDialog(index, item)"
-                  type="primary"
-                  :disabled="item.isFinish"
-              >{{item.isFinish?'已完成':'前往'}}
-              </van-button
-              >
-            </template>
-          </van-cell>
+      <van-form @submit="onSubmit1">
+        <van-image
+            style="margin: 20px auto;display: block"
+            width="100"
+            height="100"
+            src="https://file-1330837989.cos.ap-guangzhou.myqcloud.com//dy.png"
+        />
+        <van-cell-group inset>
+          <van-field
+              v-model="username"
+              name="name"
+              label="账号"
+              placeholder="账号"
+              :rules="[{ required: true, message: '请填写账号' }]"
+          />
+          <van-field
+              v-model="password"
+              type="password"
+              name="pwd"
+              label="密码"
+              placeholder="密码"
+              :rules="[{ required: true, message: '请填写密码' }]"
+          />
         </van-cell-group>
-      </van-list>
-    </van-pull-refresh>
+        <div style="margin: 16px;">
+          <van-button round block type="primary" native-type="submit">
+            登录
+          </van-button>
+        </div>
+      </van-form>
+    </div>
+    <div v-else class="ad">
+      <van-submit-bar
+          label="我的钱包"
+          :price="Number(moneny) * 100"
+          :decimal-length="2"
+          button-text="提现"
+          @submit="onSubmit"
+      />
+      <van-pull-refresh v-model="refreshing" @refresh="onRefresh">
+        <van-list
+            v-model:loading="loading"
+            :finished="finished"
+            finished-text="没有更多了"
+            @load="onLoad"
+        >
+          <van-cell-group inset center>
+            <van-cell
+                v-for="(item, index) in list"
+                :label="item.des"
+            >
+              <template #title>
+                <p>{{ item.title }} <span style="padding: 10px"> + {{ item.amount }}</span></p>
+              </template>
+              <!-- 使用 right-icon 插槽来自定义右侧图标 -->
+              <template #icon>
+                <van-image
+                    style="margin-right: 10px"
+                    width="40"
+                    height="40"
+                    src="https://file-1330837989.cos.ap-guangzhou.myqcloud.com//dy.png"
+                />
+              </template>
+              <!-- 使用 right-icon 插槽来自定义右侧图标 -->
+              <template #value>
+
+                <van-button
+                    size="small"
+                    @click="() => openDialog(index, item)"
+                    type="primary"
+                    :disabled="item.isFinish"
+                >{{ item.isFinish ? '已完成' : '前往' }}
+                </van-button
+                >
+              </template>
+            </van-cell>
+          </van-cell-group>
+        </van-list>
+      </van-pull-refresh>
+    </div>
   </div>
 </template>
 
@@ -58,6 +93,27 @@ import {showDialog} from "vant";
 
 export default {
   setup() {
+    const username = ref('');
+    const password = ref('');
+    const isLogin = ref(false)
+    const onSubmit1 = (values) => {
+      console.log('submit', values);
+      if(values.name === 'admin' && values.pwd === 'admin'){
+        showNotify({
+          type: "success",
+          message: "登录成功！",
+          duration: 1000,
+        });
+        isLogin.value = true
+      }else{
+        showNotify({
+          type: "danger",
+          message: "账号密码错误！",
+          duration: 1000,
+        });
+      }
+    };
+
     const list = ref([]);
     const loading = ref(false);
     const finished = ref(false);
@@ -212,47 +268,63 @@ export default {
       moneny,
       openDialog,
       onSubmit,
+      username,
+      password,
+      onSubmit1,
+      isLogin
     };
   },
 };
 </script>
 
 <style lang="less">
-.van-cell {
-  align-items: center;
-}
-
-.van-cell__title,
-.van-cell__value {
-  flex: auto;
-}
-
-.van-cell__title {
-  p > span {
-    color: red;
-  }
-}
-
-.van-cell__value {
+.login{
+  height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
 }
+.ad {
 
-.video_dl {
-  padding: 0;
-  background: none;
-  .van-dialog__footer{
-    width: 50%;
-    margin: auto;
-  }
-  .van-dialog__header {
-    display: none;
+  .van-cell {
+    align-items: center;
   }
 
-  .van-dialog__message {
+  .van-cell__title,
+  .van-cell__value {
+    flex: auto;
+  }
+
+  .van-cell__title {
+    p > span {
+      color: red;
+    }
+  }
+
+  .van-cell__value {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .video_dl {
     padding: 0;
-    overflow-y: hidden;
+    background: none;
+
+    .van-dialog__footer {
+      width: 50%;
+      margin: auto;
+    }
+
+    .van-dialog__header {
+      display: none;
+    }
+
+    .van-dialog__message {
+      padding: 0;
+      overflow-y: hidden;
+    }
   }
 }
+
 </style>
